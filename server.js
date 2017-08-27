39582fddae1c0cc70796a182c5c1acc0225f8c63
Natty Davis <natty.davis79@gmail.com>
2017-08-27 15:39:52 -0400

    required our initial depencies in server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+
+// 1st step in our server process is to require our dependencies
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser ');
+
+//initialize the express server
+const app = express();
+const PORT = process.argv[2] || process.env.port || 5000;
+
+
+app.listen(PORT, ()=>{
+  console.log(`ok my port is up and running and listening on ${PORT}`)
+})
